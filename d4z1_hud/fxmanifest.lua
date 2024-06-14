5805f5d436c8be61ca2e9d3ed3772c36bf8fed51
d4z1 <rcrpcz@seznam.cz>
2024-06-14 18:36:40 +0200

    Add files via upload

---
+++ d4z1_hud/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+authors "d4z1 classic fivem hud"
+
+lua54 "yes"
+
+client_scripts {
+    "client/main.lua"
+}
+
+ui_page {
+    "html/main.html"
+}
+
+files {
+    "html/main.html",
+    "html/script.js",
+    "html/style.css",
+    "html/img/*"
+}
